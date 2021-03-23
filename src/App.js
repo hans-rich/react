bfd17dd3699a21d0b718440aa612b21305fce3c2
unknown <richardson.h@outlook.com>
2021-03-23 17:46:31 +0000

    y first reaction project test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Hello from './sayHello';
 
 function App() {
+
+  const sayHello = () => {
+    console.log("hello");
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <h1>
+          Hello React world
+        </h1>
+      <Hello/>
+      </div>
   );
 }
 
